f53c76974bc88328c518fd028d8d36f98c3687ce
LeeYujeong <yujlee242@gmail.com>
2023-01-05 15:29:44 +0900

    ✨ 로그인, 회원가입 기능 추가

--- screen/My.jsx
+++ screen/My.jsx
@@ -1,6 +1,25 @@
+import { useFocusEffect } from '@react-navigation/native';
+import { useCallback } from 'react';
+import { authService } from '../firebase';
 import { View, Text } from 'react-native';
 
-export default function My() {
+export default function My({ navigation: { reset } }) {
+  useFocusEffect(
+    useCallback(() => {
+      if (!authService.currentUser) {
+        reset({
+          index: 1,
+          routes: [
+            { name: 'Tabs', params: { screen: 'Moives' } },
+            { name: 'Stacks', params: { screen: 'Login' } },
+          ],
+        });
+      }
+
+      return;
+    }, [])
+  );
+
   return (
     <View>
       <Text>My</Text>
--- util.js
+++ util.js
@@ -8,3 +8,6 @@ export const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('wi
 export const getImgPath = (path) => {
   return `https://image.tmdb.org/t/p/w500${path}`;
 };
+
+export const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+export const pwRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
